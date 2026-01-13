1f9c3894c80778986d233d866ecf2a4a1d91c9e4
TStark001 <hc2005023@gmail.com>
2026-01-13 11:07:57 +0530

    Adding LeetCode-118 to RevisionArray

---
+++ 07_RevisionArray/07_LeetCode-118.cpp
@@ -0,0 +1,40 @@
+/*
+#   LeetCode-118 :- Pascal's Triangle
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> ans;
+    for(int i = 0 ; i<numRows ; i++){
+        vector<int> row(i+1,1);
+        for(int j = 1 ; j<i ; j++){
+            row[j] = ans[i-1][j-1] + ans[i-1][j];
+        }
+        ans.push_back(row);
+    }
+    return ans;
+}
+
+int main(){
+    int numRow = 5;
+    vector<vector<int>> result = generate(numRow);
+    for(int i = 0; i < result.size(); i++){
+        for(int j = 0; j < result[i].size(); j++){
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
