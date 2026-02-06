47cf8beb181727e5303b0439bfc6eea1a57ddbc1
TStark001 <hc2005023@gmail.com>
2026-02-06 15:18:00 +0530

    Add LeetCode-145

---
+++ 20_BinaryTree/16_LeetCode-145.cpp
@@ -0,0 +1,105 @@
+/*
+#   LeetCode-145 :- Binary Tree PostOrder Traversal
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Explanation:
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+Explanation:
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+*/
+#include<iostream>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<climits>
+#include<vector>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* construct(vector<int> &v){
+    int n = v.size();
+    queue<Node*> q;
+    Node* root = new Node(v[0]);
+    q.push(root);
+    int i = 1 , j = 2;
+    while(q.size()>0 and i<n){
+        Node* temp = q.front();
+        q.pop();
+        Node *l , *r;
+        if(v[i]!=INT_MIN) l = new Node(v[i]);
+        else l = NULL;
+        if(j!=n and v[j]!=INT_MIN) r = new Node(v[j]);
+        else r = NULL;
+
+        temp->left = l;
+        temp->right = r;
+
+        if(l!=NULL) q.push(l);
+        if(r!=NULL) q.push(r);
+
+        i += 2 , j += 2;
+    }
+    return root;
+}
+void levelOrderQueue(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(q.size()>0){
+        Node* temp = q.front();
+        q.pop();
+        cout<<temp->val<<" ";
+        if(temp->left!=NULL) q.push(temp->left);
+        if(temp->right!=NULL) q.push(temp->right);
+    }
+    cout<<endl;
+}
+vector<int> preorderTraversal(Node* root) {
+    vector<int> ans;
+    stack<Node*> st;
+    if(root!=NULL) st.push(root);
+    while(st.size()>0){
+        Node* temp = st.top();
+        st.pop();
+        ans.push_back(temp->val);
+        if(temp->left!=NULL) st.push(temp->left);
+        if(temp->right!=NULL) st.push(temp->right);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
+int main(){
+    vector<int> v = {1,2,3,4,5,INT_MIN,8,INT_MIN,INT_MIN,6,7,9};
+    Node* root = construct(v);
+    cout << "Level Order Traversal: ";
+    levelOrderQueue(root);
+    vector<int> preorder = preorderTraversal(root);
+    cout << "PostOrder Traversal: ";
+    for(int x : preorder){
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
