66f68cf93be34cdca39b4fb075a059419cb41262
TStark001 <hc2005023@gmail.com>
2026-03-08 20:03:14 +0530

    LeetCode-235

---
+++ 21_BinarySearchTree/03_LeetCode-235.cpp
@@ -0,0 +1,88 @@
+/*
+#   LeetCode-235 :- Lowest Common Ancestor of Binary Seach Tree
+    Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+
+    According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+    Example 1:
+    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+    Output: 6
+    Explanation: The LCA of nodes 2 and 8 is 6.
+
+    Example 2:
+    Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+    Output: 2
+    Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+
+    Example 3:
+    Input: root = [2,1], p = 2, q = 1
+    Output: 2
+*/
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<climits>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* construct(vector<int> v){
+    int n = v.size();
+    queue<Node*> q;
+    Node* root = new Node(v[0]);
+    q.push(root);
+    int i = 1 , j = 2;
+    while(q.size()>0 and i<n){
+        Node* temp = q.front();
+        q.pop();
+        Node *l, *r;
+
+        if(v[i]!=INT_MIN) l = new Node(v[i]);
+        else l = NULL;
+        if(j!=n and v[j]!=INT_MIN) r = new Node(v[j]);
+        else r = NULL;
+
+        temp->left = l;
+        temp->right = r;
+
+        if(l!=NULL) q.push(l);
+        if(r!=NULL) q.push(r);
+
+        i += 2 , j += 2;
+    }
+    return root;
+}
+void levelOrder(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    while(q.size()>0){
+        Node* temp = q.front();
+        q.pop();
+        cout<<temp->val<<" ";
+        if(temp->left!=NULL) q.push(temp->left);
+        if(temp->right!=NULL) q.push(temp->right);
+    }
+}
+Node* lowestCommonAncestor(Node* root, Node* p, Node* q) {
+    if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+    else if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left,p,q);
+    else return root;
+}
+
+int main(){
+    vector<int> v = {6,2,8,0,4,7,9,INT_MIN,INT_MIN,3,5};
+    Node* root = construct(v);
+    Node* p = new Node(2);
+    Node* q = new Node(8);
+    Node* ans = lowestCommonAncestor(root,p,q);
+    cout<<"LCA = "<<ans->val;
+}
