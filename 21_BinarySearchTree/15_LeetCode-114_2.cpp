4b142fd72faedf5161e295555bad47ddcab36956
TStark001 <hc2005023@gmail.com>
2026-03-10 20:29:26 +0530

    LeetCode-114

---
+++ 21_BinarySearchTree/15_LeetCode-114_2.cpp
@@ -0,0 +1,113 @@
+/*
+#   LeetCode-114 :- Flatten Binary Tree to Linked List
+    Given the root of a binary tree, flatten the tree into a "linked list":
+
+    The "linked list" should use the same Node class where the right child pointer points to the next node in the list and the left child pointer is always null.
+    The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+    
+    Example 1:
+    Input: root = [1,2,5,3,4,null,6]
+    Output: [1,null,2,null,3,null,4,null,5,null,6]
+
+    Example 2:
+    Input: root = []
+    Output: []
+
+    Example 3:
+    Input: root = [0]
+    Output: [0]
+!                                           [Method-2]
+                                    Time Complexity = O(n)
+                                    Space Complexity = O(n)
+*/
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<stack>
+#include<climits>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* construct(vector<int>& v){
+    int n = v.size();
+    queue<Node*> q;
+    Node* root = new Node(v[0]);
+    q.push(root);
+    int i = 1 , j = 2;
+    while(q.size()>0 and i<n){
+        Node* temp = q.front();
+        q.pop();
+        Node *l , *r;
+
+        if(v[i]!=INT_MIN) l = new Node(v[i]);
+        else l = NULL;
+        if(j!=n and v[j]!=INT_MIN) r = new Node(v[j]);
+        else r = NULL;
+
+        temp->left = l;
+        temp->right = r;
+
+        if(l!=NULL) q.push(l);
+        if(r!=NULL) q.push(r);
+
+        i += 2 , j += 2;
+    }
+    return root;
+}
+void levelOrder(Node* root){
+    queue<Node*> q;
+    vector<string> ans;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp){
+            ans.push_back(to_string(temp->val));
+            q.push(temp->left);
+            q.push(temp->right);
+        }
+        else{
+            ans.push_back("null");
+        }
+    }
+    // remove extra nulls from the end
+    while(ans.size() > 0 && ans.back() == "null"){
+        ans.pop_back();
+    }
+    cout<<"[";
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i];
+        if(i != ans.size()-1) cout<<",";
+    }
+    cout<<"]";
+}
+void flatten(Node* root) {
+    if(root == NULL) return;
+    Node* l = root->left;
+    Node* r = root->right;
+    root->left = NULL;
+    root->right = NULL;
+    flatten(l);
+    flatten(r);
+    root->right = l;
+    Node* temp = root;
+    while(temp->right!=NULL)  temp = temp->right;
+    temp->right = r;
+}
+
+int main(){
+    vector<int> v = {1,2,5,3,4,INT_MIN,6};
+    Node* root = construct(v);
+    flatten(root);
+    levelOrder(root);
+}
