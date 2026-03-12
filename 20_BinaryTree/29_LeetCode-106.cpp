a77dea5b3f5b201c8d29fde96cc1a78f831a30ba
TStark001 <hc2005023@gmail.com>
2026-03-12 09:09:14 +0530

    LeetCode-106

---
+++ 20_BinaryTree/29_LeetCode-106.cpp
@@ -0,0 +1,111 @@
+/*
+#   LeetCode-106 :- Construct Binary Tree from Inorder and Postorder Traversal
+    Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+    Example 1:
+    Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+    Output: [3,9,20,null,null,15,7]
+
+    Example 2:
+    Input: inorder = [-1], postorder = [-1]
+    Output: [-1]
+*/
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<climits>
+using namespace std;
+
+class Node{
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* construct(vector<int>& v){
+    int n = v.size();
+    queue<Node*> q;
+    Node* root = new Node(v[0]);
+    q.push(root);
+    int i = 1 , j = 2;
+    while(q.size()>0 and i<n){
+        Node* temp = q.front();
+        q.pop();
+        Node* l;
+        Node* r;
+        if(v[i]!=INT_MIN) l = new Node(v[i]);
+        else l = NULL;
+        if(j!=n and v[j]!=INT_MIN) r = new Node(v[j]);
+
+        if(j!=n and v[j]!=INT_MIN) r = new Node(v[j]);
+        else r = NULL;
+
+        temp->left = l;
+        temp->right = r;
+
+        if(l!=NULL) q.push(l);
+        if(r!=NULL) q.push(r);
+
+        i+=2 , j+=2;
+    }
+    return root;
+}
+void levelOrder(Node* root){
+    queue<Node*> q;
+    vector<string> ans;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp){
+            ans.push_back(to_string(temp->val));
+            q.push(temp->left);
+            q.push(temp->right);
+        }
+        else{
+            ans.push_back("null");
+        }
+    }
+    // remove extra nulls from the end
+    while(ans.size() > 0 && ans.back() == "null"){
+        ans.pop_back();
+    }
+    cout<<"[";
+    for(int i=0;i<ans.size();i++){
+        cout<<ans[i];
+        if(i != ans.size()-1) cout<<",";
+    }
+    cout<<"]";
+}
+Node* build(vector<int>& in , int inlo , int inhi , vector<int>& post , int postlo , int posthi){
+    if(postlo>posthi) return NULL;
+    Node* root = new Node(post[posthi]);
+    if(postlo == posthi) return root;
+    int i = inlo;
+    while(i<=inhi){
+        if(in[i]==post[posthi]) break;
+        i++;
+    }
+    int leftCount = i - inlo;
+    int rightCount = inhi - i;
+    root->left = build(in,inlo,i-1,post,postlo,postlo+leftCount-1);
+    root->right = build(in,i+1,inhi,post,postlo+leftCount,posthi-1);
+    return root;
+}
+Node* buildTree(vector<int>& in, vector<int>& post){
+    int n = post.size();
+    return build(in,0,n-1,post,0,n-1);
+}
+
+int main(){
+    vector<int> postorder = {9,15,7,20,3};
+    vector<int> inorder = {9,3,15,20,7};
+    Node* root = buildTree(inorder, postorder);
+    levelOrder(root);
+    return 0;
+}
