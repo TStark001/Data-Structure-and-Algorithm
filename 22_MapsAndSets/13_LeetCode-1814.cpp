4cede9dabd34e8911dd64861fa5a389d4884bf2c
TStark001 <hc2005023@gmail.com>
2026-03-15 10:11:19 +0530

    LeetCode-1814

---
+++ 22_MapsAndSets/13_LeetCode-1814.cpp
@@ -0,0 +1,54 @@
+/*
+#   LeetCode-1814 :- Count Nice Pairs in an Array
+    You are given an array nums that consists of non-negative integers. Let us define rev(x) as the reverse of the non-negative integer x. For example, rev(123) = 321, and rev(120) = 21. A pair of indices (i, j) is nice if it satisfies all of the following conditions:
+
+    0 <= i < j < nums.length
+    nums[i] + rev(nums[j]) == nums[j] + rev(nums[i])
+    Return the number of nice pairs of indices. Since that number can be too large, return it modulo 109 + 7.
+
+    Example 1:
+    Input: nums = [42,11,1,97]
+    Output: 2
+    Explanation: The two pairs are:
+    - (0,3) : 42 + rev(97) = 42 + 79 = 121, 97 + rev(42) = 97 + 24 = 121.
+    - (1,2) : 11 + rev(1) = 11 + 1 = 12, 1 + rev(11) = 1 + 11 = 12.
+
+    Example 2:
+    Input: nums = [13,10,35,24,76]
+    Output: 4
+*/
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+int rev(int n){
+    int r = 0;
+    while(n>0){
+        r *= 10;
+        r += (n%10);
+        n /= 10;
+    }    return r;
+}
+int countNicePairs(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    unordered_map<int,int> m;
+    for(int i = 0 ; i<n ; i++){
+        nums[i] -= rev(nums[i]);
+    }
+    for(int i = 0 ; i<n ; i++){
+        if(m.find(nums[i])!=m.end()){
+            count = count%1000000007;
+            count += m[nums[i]];
+            m[nums[i]]++;
+        }
+        else m[nums[i]]++;
+    }
+    return count%1000000007;
+}
+
+int main(){
+    vector<int> nums = {42,11,1,97};
+    cout<<"No. of Pairs = "<<countNicePairs(nums);
+}
