c867b402a225e4de12c4a7ce4542231b7c9e9def
TStark001 <hc2005023@gmail.com>
2026-03-22 21:02:35 +0530

    HeapifyAlgorithm 0-based indexing

---
+++ 23_Heaps/14_HeapifyAlgorithm_2.cpp
@@ -0,0 +1,51 @@
+/*
+    Convert non-sorted array into minheap and print in the form of array
+    0-based indexing
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print(vector<int>& arr,int n){
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void heapify(int i , vector<int>& arr , int n){
+    while(true){
+        int left = 2*i+1 , right = 2*i+2;
+        if(left>=n) break;
+        if(right>=n){
+            if(arr[i]>arr[left]){
+                swap(arr[i],arr[left]);
+                i = left;
+            }
+            break;
+        }
+        if(arr[left]<arr[right]){
+            if(arr[i]>arr[left]){
+                swap(arr[i],arr[left]);
+                i = left;
+            }
+            else break;
+        }
+        else{
+            if(arr[i]>arr[right]){
+                swap(arr[i],arr[right]);
+                i = right;
+            }
+            else break;
+        }
+    }
+}
+
+int main(){
+    vector<int> arr = {10,2,14,11,1,4};
+    int n = arr.size();
+    print(arr,n);
+    for(int i = n/2-1 ; i>=0 ; i--){
+        heapify(i,arr,n);
+    }
+    print(arr,n);
+}
