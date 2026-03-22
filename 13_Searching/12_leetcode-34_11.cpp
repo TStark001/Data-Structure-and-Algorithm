11ae9f4ac6ba86efbfe24210d907d806ae1ee4db
TStark001 <hc2005023@gmail.com>
2026-03-22 19:49:09 +0530

    LeetCode-34

--- 13_Searching/12_leetcode-34_11.cpp
+++ 13_Searching/12_leetcode-34_11.cpp
@@ -19,8 +19,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> searchrange(vector<int> &nums, int x)
-{
+vector<int> searchrange(vector<int> &nums, int x){
     vector<int> v(2);
     int n = nums.size();
     int lo = 0;
@@ -28,72 +27,44 @@ vector<int> searchrange(vector<int> &nums, int x)
     v[0] = -1;
     v[1] = -1;
     //* first occurrence
-    while (lo <= hi)
-    {
+    while (lo <= hi){
         int mid = lo + (hi - lo) / 2;
-        if (nums[mid] == x)
-        {
-            if (mid == 0)
-            {
+        if (nums[mid] == x){
+            if (mid == 0){
                 v[0] = mid;
                 break;
             }
-            else if (nums[mid - 1] != x)
-            {
+            else if (nums[mid - 1] != x){
                 v[0] = mid;
                 break;
             }
-            else
-            {
-                hi = mid - 1;
-            }
+            else hi = mid - 1;
         }
-        else if (nums[mid] > x)
-        {
-            hi = mid - 1;
-        }
-        else
-        {
-            lo = mid + 1;
-        }
+        else if (nums[mid] > x) hi = mid - 1;
+        else lo = mid + 1;
     }
-
     //$ last occurrence
     lo = 0;
     hi = n - 1;
-    while (lo <= hi)
-    {
+    while (lo <= hi){
         int mid = lo + (hi - lo) / 2;
-        if (nums[mid] == x)
-        {
-            if (mid == n - 1)
-            {
+        if (nums[mid] == x){
+            if (mid == n - 1){
                 v[1] = mid;
                 break;
             }
-            else if (nums[mid + 1] != x)
-            {
+            else if (nums[mid + 1] != x){
                 v[1] = mid;
                 break;
             }
-            else
-            {
-                lo = mid + 1;
-            }
+            else lo = mid + 1;
         }
-        else if (nums[mid] > x)
-        {
-            hi = mid - 1;
-        }
-        else
-        {
-            lo = mid + 1;
-        }
+        else if (nums[mid] > x) hi = mid - 1;
+        else lo = mid + 1; 
     }
     return v;
 }
-int main()
-{
+int main(){
     vector<int> nums = {5, 7, 7, 8, 8, 10};
     int x = 8;
     vector<int> result = searchrange(nums, x);
