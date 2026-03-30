88026158f6d30eecd4ca765e280796bacf20af31
TStark001 <hc2005023@gmail.com>
2026-03-30 21:04:14 +0530

    LeetCode-215

--- 23_Heaps/04_LeetCode-215.cpp
+++ 23_Heaps/04_LeetCode-215.cpp
@@ -17,7 +17,7 @@
 #include<queue>
 using namespace std;
 
-int findKthLargest(vector<int>& nums, int k) {
+int findKthLargest(vector<int>& nums, int k){
     priority_queue<int,vector<int>,greater<int>> pq; // minHeap
     for(int i = 0 ; i<nums.size() ; i++){
         pq.push(nums[i]);
