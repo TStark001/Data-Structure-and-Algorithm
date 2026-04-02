493e82cdf4aa330f31ae7cfc594c4fd50ad73fa2
TStark001 <hc2005023@gmail.com>
2026-04-02 20:53:51 +0530

    Minimum Product Subset

---
+++ 24_Greedy/05_MinimumProductSubset.cpp
@@ -0,0 +1,50 @@
+//  Minimum Product subsets
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+
+int minProduct(vector<int>& arr){
+    int cn = 0; // count of negative
+    int cz = 0; // count of zero
+    int cp = 0; // count of positive
+    int prod_pos = 1; // product of positive
+    int prod_neg = 1; // product of negative
+    int largestNegative = INT_MIN;
+    for(int i = 0 ; i<arr.size() ; i++){
+        if(arr[i] < 0){
+            cn++;
+            prod_neg *= arr[i];
+            largestNegative = max(largestNegative,arr[i]);
+        }
+        if(arr[i] == 0) cz++;
+        if(arr[i] > 0) {
+            cp++;
+            prod_pos *= arr[i];
+        }
+    }
+    if(cn == 0){
+        if(cz > 0) return 0;
+        else{
+            auto it = min_element(arr.begin(),arr.end());
+            return *it;
+        }
+    }
+    else{
+        if(cn % 2 == 0){
+            // even
+            return (prod_neg / largestNegative) * prod_pos;
+        }
+        else{
+            // odd
+            return prod_neg * prod_pos;
+        }
+    }
+}
+
+int main(){
+    vector<int> arr = {-2,-3,1,4,-2,-5};
+    cout<<minProduct(arr);
+    return 0;
+}
