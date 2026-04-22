a299671b7bbccce772bdf48ab87d95c07aa422e5
TStark001 <hc2005023@gmail.com>
2026-04-22 18:04:05 +0530

    Dice Combination (Recursion + Memoization)

---
+++ 25_DynamicProgramming/15_diceCombination_1.cpp
@@ -0,0 +1,45 @@
+/*
+#   Dice Combination
+    Your task is to count the number of ways to construct sum n by throwing a dice one or more times.
+    Each throw produces an outcome between 1 and 6.
+
+    For example , if n = 3 , there are 4 ways:
+        . 1 + 1 + 1 
+        . 1 + 2
+        . 2 + 1
+        . 3
+    
+    Input :- The only input line has an integer n
+
+    Output :- Print the number of ways modulo 10^9 + 7
+    
+    Example:-
+    Input :- 3
+    Output :- 4
+$                       Recursion + Memoization
+*/
+#include<iostream>
+#include<vector>
+#define mod 1000000007  
+#define ll long long int
+using namespace std;
+
+vector<ll> dp;
+ll noOfWays(int n){
+    if(n == 0) return 1;
+    if(dp[n] != -1) return dp[n];
+    ll sum = 0;
+    for(int i = 1 ; i<= 6 ; i++){
+        if(n - i < 0) break;
+        sum = (sum % mod + noOfWays(n-i) % mod) % mod;
+    }
+    return dp[n] = sum % mod;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    dp.clear();
+    dp.resize(n+5 , -1);
+    cout<<noOfWays(n);
+}
